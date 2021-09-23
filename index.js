241fc4c81012dd598f7335bd09991fc1dc608dda
Dmitry Velichko <soleil-2005@mail.ru>
2021-09-23 14:38:35 +0300

    New commit

--- index.js
+++ index.js
@@ -4,77 +4,81 @@
 
 // 1 строка: делаю
 
-function verbs(verb, pronoun) {
+const btn = document.querySelector(".btn");
 
-    let verbEnd = verb.slice(-3, verb.length);
-    let verb2 = verb.slice(0, -3);
-   
-    if( verbEnd === "ать" ||  verbEnd === "еть" || verbEnd === "ять" || verbEnd === "уть" || verbEnd === "ють" || verbEnd === "ыть" || verbEnd === "оть") {
-  
-  
+btn.addEventListener("click", () => {
+  alert(verbs("делать", "я"));
+});
+
+function verbs(verb, pronoun) {
+  let verbEnd = verb.slice(-3, verb.length);
+  let verb2 = verb.slice(0, -3);
+
+  if (
+    verbEnd === "ать" ||
+    verbEnd === "еть" ||
+    verbEnd === "ять" ||
+    verbEnd === "уть" ||
+    verbEnd === "ють" ||
+    verbEnd === "ыть" ||
+    verbEnd === "оть"
+  ) {
     if (pronoun === "я") {
-      console.log(verb2 + "аю");
+      return verb2 + "аю";
     }
-  
+
     if (pronoun === "ты") {
-      console.log(verb2 + "ешь");
+      return verb2 + "ешь";
     }
-  
+
     if (pronoun === "он") {
-      console.log(verb2 + "ает");
+      return verb2 + "ает";
     }
-  
+
     if (pronoun === "она") {
-      console.log(verb2 + "ает");
+      return verb2 + "ает";
     }
-  
+
     if (pronoun === "мы") {
-      console.log(verb2 + "аем");
+      return verb2 + "аем";
     }
-  
+
     if (pronoun === "вы") {
-      console.log(verb2 + "аете");
+      return verb2 + "аете";
     }
-  
+
     if (pronoun === "они") {
-      console.log(verb2 + "ают");
+      return verb2 + "ают";
     }
   }
-  
-  if( verbEnd === "ить") {
-  
-  
-      if (pronoun === "я") {
-        console.log(verb2 + "у");
-      }
-    
-      if (pronoun === "ты") {
-        console.log(verb2 + "ишь");
-      }
-    
-      if (pronoun === "он") {
-        console.log(verb2 + "ит");
-      }
-    
-      if (pronoun === "она") {
-        console.log(verb2 + "ит");
-      }
-    
-      if (pronoun === "мы") {
-        console.log(verb2 + "им");
-      }
-    
-      if (pronoun === "вы") {
-        console.log(verb2 + "ите");
-      }
-    
-      if (pronoun === "они") {
-        console.log(verb2 + "ят");
-      }
+
+  if (verbEnd === "ить") {
+    if (pronoun === "я") {
+      return verb2 + "у";
     }
-  
-    
+
+    if (pronoun === "ты") {
+      return verb2 + "ишь";
+    }
+
+    if (pronoun === "он") {
+      return verb2 + "ит";
+    }
+
+    if (pronoun === "она") {
+      return verb2 + "ит";
+    }
+
+    if (pronoun === "мы") {
+      return verb2 + "им";
+    }
+
+    if (pronoun === "вы") {
+      return verb2 + "ите";
+    }
+
+    if (pronoun === "они") {
+      return verb2 + "ят";
+    }
   }
-  
-  alert(verbs("сушить", "я"));
-  
+}
