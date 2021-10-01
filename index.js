5a8d85e9a2b7dd3bef506b40fbd6cffc9b8e5cde
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-01 21:58:53 +0300

    New commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const arr = [[2,1,3], [3,2,1]]
+
 const sortMatrix = (arr) => {
-    const sortedNumbers = arr.flat().sort((a, b) => a - b);
-    const rowsCount = arr.length;
-    const colsCount = arr[0].length; 
-    const result = []; 
-  };
-  
+  const sortedNumbers = arr.flat().sort((a, b) => a - b);
+  const rowsCount = arr.length;
+  const colsCount = arr[0].length;
+  const result = [];
+
+  for (let i = 0; i < rowsCount; i++) {
+    const newRow = [];
+
+    for (let j = 0; j < colsCount; j++) {
+      newRow.push(sortedNumbers[j * rowsCount + i]);
+    }
+
+    result.push(newRow);
+  }
+
+  return result;
+};
+
