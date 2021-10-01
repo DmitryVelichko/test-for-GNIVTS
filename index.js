34cef9e894db4f5149fec5fcf65f87f4c4deeec0
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-01 21:54:27 +0300

    New commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sortMatrix = (arr) => {
     const sortedNumbers = arr.flat().sort((a, b) => a - b);
     const rowsCount = arr.length;
+    const colsCount = arr[0].length; 
+    const result = []; 
   };
   
