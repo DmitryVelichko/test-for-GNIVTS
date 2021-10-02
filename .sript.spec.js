1a143c238a512c7461bb0e39369e45f26c43a5e1
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-02 21:00:24 +0300

    New Commit

--- .sript.spec.js
+++ .sript.spec.js
@@ -4,4 +4,10 @@ describe("Function sleep", () => {
   it("should be defined", () => {
     expect(sleep).toBeDefined();
   });
+
+  it('should be function', () => {
+    expect(typeof sleep).toBe('function');
+});
+
+
 });
