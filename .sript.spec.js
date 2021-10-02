1d4867d551fa86fc583c6091a8443be6a4aeca77
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-02 21:17:39 +0300

    New Commit

--- .sript.spec.js
+++ .sript.spec.js
@@ -1,26 +1,32 @@
-import sleep from "./script.js";
+import sleep, { delayPrint } from "./index.js";
 
 describe("Function sleep", () => {
   it("should be defined", () => {
     expect(sleep).toBeDefined();
   });
 
-  it('should be function', () => {
-    expect(typeof sleep).toBe('function');
-});
+  it("should be function", () => {
+    expect(typeof sleep).toBe("function");
+  });
 
-it('should return Promise', () => {
+  it("should return Promise", () => {
     expect(sleep(1000)).toBeInstanceOf(Promise);
-});
+  });
 
-it('should return "Delayed print"', () => {
-    expect(sleep()).resolves.toBe('Delayed print')
-});
+  it('should return "Delayed print"', () => {
+    expect(sleep()).resolves.toBe("Delayed print");
+  });
 
-it('should return Promise after 1 sec', async () => {
+  it("should return Promise after 1 sec", async () => {
     const start = new Date();
     await sleep(1000);
-    const end = new Date()
+    const end = new Date();
     expect(end - start).toBeCloseTo(1000, -2);
+  });
 });
+
+describe("Функция sleep", () => {
+  it("должна вернуть promise", () => {
+    expect(sleep(1000)).resolves;
+  });
 });
