5efc8b12b95d9bf6f3c945f94455f9cc6953dc51
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-02 21:00:07 +0300

    New Commit

--- .sript.spec.js
+++ .sript.spec.js
@@ -1,3 +1,7 @@
+import sleep from "./script.js";
 
-import sleep from './script.js'
-
+describe("Function sleep", () => {
+  it("should be defined", () => {
+    expect(sleep).toBeDefined();
+  });
+});
