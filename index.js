1c01afb0adfb432f2e0fd25d44175b75fa1befeb
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-03 23:54:31 +0300

    New commit

--- index.js
+++ index.js
@@ -43,5 +43,11 @@
          sortRowWise(m);
   
          let numbers = [0, 1 , 2, 3, 10, 20, 30 ];
+         numbers.sort( function( a , b){
+            if(a > b) return 1;
+            if(a < b) return -1;
+            return 0;
+        });
         
+       
  </script>
