6a17816a81c6bf5d1df029b5f1954e25b784bca8
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-03 23:51:44 +0300

    New commit

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
+
 <script>
  
-// JavaScript Program to sort 2D matrix row-wise
- 
-    function sortRowWise(m)
-    {
-       // loop for rows of matrix
-       for (let i = 0; i < m.length; i++) {
-  // loop for column of matrix
-  for (let j = 0; j < m[i].length; j++) {
-
-    // loop for comparison and swapping
-    for (let k = 0; k < m[i].length - j - 1; k++) {
-        
-    }
-}
-        
-    }
-
-    // printing the sorted matrix
-    for (let i = 0; i < m.length; i++) {
-        for (let j = 0; j < m[i].length; j++)
-            document.write(m[i][j] + " ");
-        document.write("<br/>");
-    }
-
-    return 0;
-    }
- 
-// Driver code
- 
-        let m = [[ 9, 8, 7, 1 ],
-                      [ 7, 3, 0, 2 ],
-                      [ 9, 5, 3, 2 ],
-                      [ 6, 3, 1, 2 ]];
-        sortRowWise(m);
-
-</script>
+ // JavaScript Program to sort 2D matrix row-wise
+  
+     function sortRowWise(m)
+     {
+         // loop for rows of matrix
+         for (let i = 0; i < m.length; i++) {
+   
+             // loop for column of matrix
+             for (let j = 0; j < m[i].length; j++) {
+   
+                 // loop for comparison and swapping
+                 for (let k = 0; k < m[i].length - j - 1; k++) {
+                     if (m[i][k] > m[i][k + 1]) {
+   
+                         // swapping of elements
+                         let t = m[i][k];
+                         m[i][k] = m[i][k + 1];
+                         m[i][k + 1] = t;
+                     }
+                 }
+             }
+         }
+   
+         // printing the sorted matrix
+         for (let i = 0; i < m.length; i++) {
+             for (let j = 0; j < m[i].length; j++)
+                 document.write(m[i][j] + " ");
+             document.write("<br/>");
+         }
+   
+         return 0;
+     }
+  
+ // Driver code
+  
+         let m = [[ 9, 8, 7, 1 ],
+                       [ 7, 3, 0, 2 ],
+                       [ 9, 5, 3, 2 ],
+                       [ 6, 3, 1, 2 ]];
+         sortRowWise(m);
+  
+ // This code is contributed by sanjoy_62.
+ </script>
