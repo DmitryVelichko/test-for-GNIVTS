3cb11d6efe82f9d3779900d6840953cc17763b06
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-04 13:19:15 +0300

    New commit

--- index.ts
+++ index.ts
@@ -5,3 +5,14 @@ type Status = 'idle' | 'loading' | 'response'; // union
 let myStatus: Status = "idle";
 
 let y: (number | string) = 1;
+
+const b: number[] = [1,2,3];
+
+const c = [1,2,3,'dfdf'];
+
+interface PersonInterface {
+    id: number
+    name: string
+    register(): string
+  }
+  
