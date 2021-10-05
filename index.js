d8a6124604fc3536769c50662e31d2fe673b6175
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-05 17:13:50 +0300

    new commit

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const geo = require('./geo.json');
 
 const listOfCaption = _.map(geo, 'caption');
 
+const federalDistricts = _.reduce(geo, (acc, cur) => {
+	acc[cur.code] = cur.shortCaption;
+	return acc;
+}, {});
+
