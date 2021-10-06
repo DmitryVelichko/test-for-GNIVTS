7bebed9029e45d7aa8cd0387d591cc71adc038aa
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-06 16:18:05 +0300

    New Commit

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-const _ = require('lodash');
-const geo = require('./geo.json');
+const _ = require("lodash");
+const geo = require("./geo.json");
 
-const listOfCaption = _.map(geo, 'caption');
-
-const federalDistricts = _.reduce(geo, (acc, cur) => {
-	acc[cur.code] = cur.shortCaption;
-	return acc;
-}, {});
-
-const numberOfDistricts = _.size(geo);
-
-const sortOfDictrict = _.sortBy(geo,  (geo) => {
-	return geo.code
-});
-
-
-console.log(listOfCaption);
-console.log(federalDistricts);
-console.log(numberOfDistricts);
-console.log(sortOfDictrict);
