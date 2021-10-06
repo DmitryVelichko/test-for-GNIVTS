64babbd541a8e58aa45524f7de3f8dd03b94b07c
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-06 16:28:20 +0300

    New Commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+// Lodash
 const _ = require("lodash");
+
+// Коды
 const geo = require("./geo.json");
 
+// Доходы
 const incomings = require("./incomings.json");
 
 // Из массива geo сделать объект "Федеральные округа" - ключ code (числовое значение), значение caption (название федерального округа)
@@ -33,26 +38,25 @@ console.log(createRegionsObj());
 // Сформировать отчет report2020 в виде объекта, где ключ - название региона, а значение - число из ключа value
 
 const createReport = () => {
-	const obj = {};
-  
-	_.forEach(incomings["2020"].regions, function (item) {
-	  return (obj[item.code] = item.value);
-	});
-  
-	const finalObj = {};
-  
-	for (var key in obj) {
-		if (regions[key]) {
-		  finalObj[regions[key]] = obj[key];
-		} else {
-			finalObj[key] = obj[key];
-		}
-	  }
-	
-	  return finalObj;
-  };
-  
-  // Отчет report2020
-  
-  const report2020 = createReport();
-  
+  const obj = {};
+
+  _.forEach(incomings["2020"].regions, function (item) {
+    return (obj[item.code] = item.value);
+  });
+
+  const finalObj = {};
+
+  for (var key in obj) {
+    if (regions[key]) {
+      finalObj[regions[key]] = obj[key];
+    } else {
+      finalObj[key] = obj[key];
+    }
+  }
+
+  return finalObj;
+};
+
+// Отчет report2020
+
+const report2020 = createReport();
