82137e90b4789881abc09e18ae503b0c234d615a
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-06 16:21:21 +0300

    New Commit

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const geo = require("./geo.json");
 
 const incomings = require("./incomings.json");
 
+// Из массива geo сделать объект "Федеральные округа" - ключ code (числовое значение), значение caption (название федерального округа)
+const createFederalDistricsObj = () => {
+	const obj = {};
+	
+	return obj;
+  };
+  
+  
