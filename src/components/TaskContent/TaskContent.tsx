fa4426547eddcacaa82313ddd701c4be1ebee9eb
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-09 09:50:53 +0300

    New commit

--- src/components/TaskContent/TaskContent.tsx
+++ src/components/TaskContent/TaskContent.tsx
@@ -2,7 +2,30 @@ import { Button, UserAvatar, UserProfile } from '@ff/ui-kit';
 import React from 'react';
 
 const TaskContent = () => (
- 
+  <>
+    <div
+      style={{
+        height: '450px',
+        overflow: 'auto',
+        fontSize: '14px',
+        paddingRight: '10px',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+          fontSize: '20px',
+        }}
+      >
+        dwayne johnson
+        <UserAvatar
+          size="medium"
+          imageUrl="https://i.pinimg.com/236x/09/a8/f9/09a8f927bf9dd1f193874a0790e13019.jpg"
+        />
+    
+  </>
 );
 
 export default TaskContent;
