0d798f019e1e8969da2533413e0e8361588f2427
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-09 09:42:59 +0300

    New commit

---
+++ src/stores/diagrams.ts
@@ -0,0 +1,10 @@
+import { makeAutoObservable } from 'mobx';
+import _ from 'lodash';
+
+import DiagramValue from '../models/DiagramValue';
+
+class Diagrams {
+  
+}
+
+export default new Diagrams();
