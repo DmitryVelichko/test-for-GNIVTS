09ba09b6f82edaafe20b3e35f14544bc7a2a7cb1
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-09 09:51:25 +0300

    New commit

--- src/components/TaskContent/TaskContent.tsx
+++ src/components/TaskContent/TaskContent.tsx
@@ -24,7 +24,43 @@ const TaskContent = () => (
           size="medium"
           imageUrl="https://i.pinimg.com/236x/09/a8/f9/09a8f927bf9dd1f193874a0790e13019.jpg"
         />
-    
+      </div>
+     
+      <div>набор файлов-вложений</div>
+      <div>
+        выбранный исполнитель(если есть):
+        <UserProfile userName="dwayne johnson">
+          <UserAvatar
+            size="small"
+            imageUrl="https://i.pinimg.com/236x/09/a8/f9/09a8f927bf9dd1f193874a0790e13019.jpg"
+          />
+        </UserProfile>
+      </div>
+      <div>
+        список претендентов-исполнителей:
+        <div style={{ display: 'flex' }}>
+          {Array(5).fill(
+            <UserAvatar
+              size="small"
+              imageUrl="https://i.pinimg.com/236x/09/a8/f9/09a8f927bf9dd1f193874a0790e13019.jpg"
+            />,
+          )}
+        </div>
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '20px',
+          width: 'calc(100% - 86px)',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Button type="primary">Хочу это сделать</Button> <br />
+        <div>{15} просмотров</div>
+      </div>
+    </div>
   </>
 );
 
