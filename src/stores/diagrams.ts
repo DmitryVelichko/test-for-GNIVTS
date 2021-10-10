612317d43d48433ac3adbc3f9922726a23a4a898
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-10 14:32:49 +0300

    New commit

--- src/stores/diagrams.ts
+++ src/stores/diagrams.ts
@@ -1,7 +1,7 @@
-import { makeAutoObservable } from 'mobx';
-import _ from 'lodash';
+import { makeAutoObservable } from "mobx";
+import _ from "lodash";
 
-import DiagramValue from '../models/DiagramValue';
+import DiagramValue from "../models/DiagramValue";
 
 class Diagrams {
   chartData: DiagramValue[] = [];
@@ -15,11 +15,14 @@ class Diagrams {
     _.times(4, (i) => {
       const randomValue = Math.round(Math.random() * 100);
       const randomColor = `#${Math.random().toString(16).slice(2, 8)}`;
-
-      
+      this.chartData.push(
+        new DiagramValue({
+          value: randomValue,
+          color: randomColor,
+        })
+      );
     });
   }
-  
 }
 
 export default new Diagrams();
