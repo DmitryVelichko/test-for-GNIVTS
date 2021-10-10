6664547b588a459d35a75eb6f2230420a8b43566
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-10 14:31:57 +0300

    New commit

--- src/stores/diagrams.ts
+++ src/stores/diagrams.ts
@@ -5,7 +5,13 @@ import DiagramValue from '../models/DiagramValue';
 
 class Diagrams {
   chartData: DiagramValue[] = [];
+  constructor() {
+    makeAutoObservable(this);
 
+    this.setRandomData();
+  }
+
+ 
   
 }
 
