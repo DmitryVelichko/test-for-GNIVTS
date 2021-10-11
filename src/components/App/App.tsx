30b7bc360493171b62358491db8c1706e1528379
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-11 09:42:33 +0300

    New Commmit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,28 @@
-import React, { FC, useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { FC, useState, useEffect } from "react";
+import axios from "axios";
 
-import classes from './App.module.scss';
-import Posts from '../Posts';
-import Search from '../Search';
-import Loading from '../Loading';
-import Error from '../Error';
-import { Post } from '../../data/posts';
-import { useLocalStorage } from '../../hook';
-import Table from '../Table';
+import classes from "./App.module.scss";
+import Posts from "../Posts";
+import Search from "../Search";
+import Loading from "../Loading";
+import Error from "../Error";
+import { Post } from "../../data/posts";
+import { useLocalStorage } from "../../hook";
+import Table from "../Table";
 
 const App: FC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [search, setSearch] = useLocalStorage('search', '');
+  const [search, setSearch] = useLocalStorage("search", "");
   const [isLoading, setLoading] = useState(true);
   const [isError, setError] = useState(false);
-  const [isView, setView] = useState('list');
+  const [isView, setView] = useState("list");
 
-  const handleSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => setSearch(e.target.value);
+  const handleSearch: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+    setSearch(e.target.value);
 
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      .get("https://jsonplaceholder.typicode.com/posts?_limit=5")
       .then((res) => {
         setPosts(res.data);
         setLoading(false);
@@ -37,11 +38,27 @@ const App: FC = () => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.btns}>
-        <button className={classes.button} type="button" value="list" onClick={() => setView('list')}>Список</button>
-        <button className={classes.button} type="button" value="table" onClick={() => setView('table')}>Таблица</button>
+        <button
+          className={classes.button}
+          type="button"
+          value="list"
+          onClick={() => setView("list")}
+        >
+          Список
+        </button>
+        <button
+          className={classes.button}
+          type="button"
+          value="table"
+          onClick={() => setView("table")}
+        >
+          Таблица
+        </button>
       </div>
 
-     
+      <Search search={search} handleSearch={handleSearch} />
+    
+  );
 };
 
 export default App;
