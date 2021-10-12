dcb4cfcb3740fbe1300af0ab97cc7335f5a4eff5
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-12 10:51:07 +0300

    New commit

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -10,6 +10,9 @@ describe('Search component works correctly', () => {
     const onChange = jest.fn();
     render(<Search search="" handleSearch={onChange} />);
 
-    
+    await userEvent.type(screen.getByRole('textbox'), 'React');
+
+    expect(onChange).toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledTimes(5);
   });
 });
