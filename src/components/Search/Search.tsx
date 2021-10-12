4ce70fcf0654a898988c156fc7112d3ba9d4fce0
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-12 10:52:13 +0300

    New commit

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+import classes from './Search.module.scss';
+
+type SearchProps = {
+  search: string;
+  handleSearch: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
+};
+
+const Search: FC<SearchProps> = (props) => {
+  const { search, handleSearch, placeholder = 'Введите текст...' } = props;
+  return <input className={classes.search} type="text" placeholder={placeholder} value={search} onChange={handleSearch} />;
+};
+
+export default Search;
