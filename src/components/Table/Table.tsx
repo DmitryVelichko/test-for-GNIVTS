5bf91a944d6582baf338f5326a32d5917e6a8676
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-13 10:59:50 +0300

    New commit

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -8,6 +8,22 @@ type TableProps = {
   search: string;
 };
 
+const Table: FC<TableProps> = (props) => {
+  const { data, search = '' } = props;
 
+  const [filtered, setFiltered] = useState<PostProps[]>(data);
+
+  useEffect(() => {
+    if (search.length > 2) {
+      const filteredPosts = data.filter((post) => post.title.toLowerCase().includes(search.toLowerCase()));
+
+      setFiltered(filteredPosts);
+    } else {
+      setFiltered(data);
+    }
+  }, [search]);
+
+
+};
 
 export default Table;
