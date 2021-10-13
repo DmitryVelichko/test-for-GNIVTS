a9c1cbd4036f9e4e414cb241698602c8723d608d
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-13 11:00:19 +0300

    New commit

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from "react";
 
-import { PostProps } from '../Posts/Post';
-import classes from './Table.module.scss';
+import { PostProps } from "../Posts/Post";
+import classes from "./Table.module.scss";
 
 type TableProps = {
   data: PostProps[];
@@ -9,13 +9,15 @@ type TableProps = {
 };
 
 const Table: FC<TableProps> = (props) => {
-  const { data, search = '' } = props;
+  const { data, search = "" } = props;
 
   const [filtered, setFiltered] = useState<PostProps[]>(data);
 
   useEffect(() => {
     if (search.length > 2) {
-      const filteredPosts = data.filter((post) => post.title.toLowerCase().includes(search.toLowerCase()));
+      const filteredPosts = data.filter((post) =>
+        post.title.toLowerCase().includes(search.toLowerCase())
+      );
 
       setFiltered(filteredPosts);
     } else {
@@ -23,7 +25,24 @@ const Table: FC<TableProps> = (props) => {
     }
   }, [search]);
 
-
+  return (
+    <table className={classes.table}>
+      <thead className={classes.thead}>
+        <tr className={classes.tr}>
+          <td className={classes.td}>Title</td>
+          <td className={classes.td}>Body</td>
+        </tr>
+      </thead>
+      <tbody>
+        {filtered.map((row) => (
+          <tr key={row.id} className={classes.tr}>
+            <td className={classes.td}>{row.title}</td>
+            <td className={classes.td}>{row.body}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
