7b83a02b84f0fe636b58e1c183409e6525a1fd7b
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-14 10:01:14 +0300

    New Commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,59 +1,12 @@
-import React, { FC, useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { FC, useState, useEffect } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
-import classes from './App.module.scss';
-import Posts from '../Posts';
-import Search from '../Search';
-import Loading from '../Loading';
-import Error from '../Error';
-import { Post } from '../../data/posts';
-import { useLocalStorage } from '../../hook';
-import Table from '../Table';
+import classes from "./App.module.scss";
+import Home from "../routes/Home";
+import NotFound from "../routes/NotFound";
+import Contacts from "../routes/Contacts";
+import PostItem from "../routes/PostItem";
 
-const App: FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [search, setSearch] = useLocalStorage('search', '');
-  const [isLoading, setLoading] = useState(true);
-  const [isError, setError] = useState(false);
-  const [isView, setView] = useState('list');
 
-  const handleSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => setSearch(e.target.value);
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts?_limit=5')
-      .then((res) => {
-        setPosts(res.data);
-        setLoading(false);
-      })
-      .catch(() => {
-        setError(true);
-      });
-  }, []);
-
-  if (isError) return <Error />;
-  if (isLoading) return <Loading />;
-
-  return (
-    <div className={classes.wrapper}>
-      <div className={classes.btns}>
-        <button className={classes.button} type="button" value="list" onClick={() => setView('list')}>Список</button>
-        <button className={classes.button} type="button" value="table" onClick={() => setView('table')}>Таблица</button>
-      </div>
-
-      <Search search={search} handleSearch={handleSearch} />
-      {isView === 'list' && (
-        <div>
-          <Posts data={posts} search={search} />
-        </div>
-      )}
-      {isView === 'table' && (
-      <div>
-        <Table data={posts} search={search} />
-      </div>
-      )}
-    </div>
-  );
-};
 
 export default App;
