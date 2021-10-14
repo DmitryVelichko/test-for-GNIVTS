ced06bbe34af91f319f654eef0923fb495097d2b
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-14 10:13:19 +0300

    New Commit

--- src/components/Posts/Post.test.tsx
+++ src/components/Posts/Post.test.tsx
@@ -7,5 +7,13 @@ describe('Post component works correctly', () => {
   it('Post render', () => {
     render(<Post title="Title 1" />);
 
-   
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByText('Title 1')).toBeInTheDocument();
+    expect(screen.getByText(/Title/)).toBeInTheDocument();
+  });
+  it('Post render with body', () => {
+    render(<Post title="Title 1" body="123" />);
+
+    expect(screen.getByText('123')).toBeInTheDocument();
+  });
 });
