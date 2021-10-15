4cffae14fdaf0d31e862231a38321bb534dff9ab
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-15 09:26:22 +0300

    new commit

--- src/hook/useToggle.ts
+++ src/hook/useToggle.ts
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function useToggle(initialValue: boolean): [boolean, () => void] {
   const [value, setValue] = useState(initialValue);
 
-  
+  const toggle = () => {
+    setValue(!value);
+  };
+  return [value, toggle];
 }
 
 // eslint-disable-next-line
