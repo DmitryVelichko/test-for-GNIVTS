ed7f409c28a0ca120608fbc2538330d850c370e3
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-15 09:25:29 +0300

    new commit

--- src/hook/useLocalStorage.ts
+++ src/hook/useLocalStorage.ts
@@ -5,7 +5,16 @@ function useLocalStorage<T>(
   initialValue: T,
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
   const getValue = () => {
-    
+    const storage = localStorage.getItem(key); // string или null
+    if (storage) {
+      return JSON.parse(storage);
+    }
+    return initialValue;
+  };
+
+  const [value, setValue] = useState<T>(getValue);
+
+
 }
 
  
