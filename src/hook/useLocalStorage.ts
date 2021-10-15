1246db4f24dab5311789788523450bc0d1b0eb38
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-15 09:25:09 +0300

    new commit

--- src/hook/useLocalStorage.ts
+++ src/hook/useLocalStorage.ts
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react';
 
 function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const getValue = () => {
+    
+}
+
  
 // eslint-disable-next-line
 export { useLocalStorage };
