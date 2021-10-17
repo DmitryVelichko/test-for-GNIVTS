e7a9d4636528892e1e8d77c44d9f561e18d6877f
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-17 10:53:47 +0300

    new commit

---
+++ src/data/postsArr.ts
@@ -0,0 +1,29 @@
+export type PostArr = {
+  id?: number;
+  title: string;
+  body?: string;
+};
+
+export const postsArr = [
+  {
+    id: 1,
+    name: 'Иван Петров',
+    email: 'ivanpetrov@mail.ru',
+  },
+  {
+    id: 2,
+    name: 'Иван Иванов',
+    email: 'ivanivanov@mail.ru',
+  },
+  {
+    id: 3,
+    name: 'Василий Сидоров',
+    email: 'vasiliy@mail.ru',
+  },
+  {
+    id: 4,
+    name: 'Петр Никитин',
+    email: 'petr@mail.ru',
+  },
+ 
+];
