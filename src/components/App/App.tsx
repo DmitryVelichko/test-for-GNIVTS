4f307b4bdabec1ca03040d2f24b6abdc1ff346a1
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-21 12:24:03 +0300

    New commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from '../routes/Home';
+import About from '../routes/About';
+import Header from '../bars/Header';
+import classes from './App.module.scss';
 
+const NotFound = React.lazy(() => import('../routes/NotFound'));
+
+const App: React.FC = () => (
+  
+);
 
 export default App;
