0a8f3db44feeb9b45dbe50026553546ed5266fd1
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-21 12:20:02 +0300

    New commit

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -7,6 +7,19 @@ const Error: React.FC<ErrorProps> = ({ code = 404 }) => {
     404: '404 Not Found',
   };
 
-}
+  return (
+    <div className={classes.error_page}>
+      {errors[code]}
+    </div>
+  );
+};
+
+type Errors = {
+  [key: string]: string;
+};
+
+type ErrorProps = {
+  code?: number;
+};
 
 export default Error;
