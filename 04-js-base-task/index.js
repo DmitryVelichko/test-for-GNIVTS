eb78dac6df7556b6dd56d18b2f01851ad3825294
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-23 13:02:13 +0300

    new commit

--- 04-js-base-task/index.js
+++ 04-js-base-task/index.js
@@ -1,7 +1,8 @@
 module.exports = function sleep(seconds) {
   new Promise((res) => setTimeout(res, seconds));
+};
+
+async function delayPrint() {
+  await sleep(1000);
+  console.log('Delayed print');
 }
-
-
-
-
