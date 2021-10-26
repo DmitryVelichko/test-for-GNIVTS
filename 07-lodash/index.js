b6613bde35ecf2cfb17a04c1ca8e21b2499d2a9c
Dmitry Velichko <soleil-2005@mail.ru>
2021-10-26 18:07:58 +0300

    New commit

---
+++ 07-lodash/index.js
@@ -0,0 +1,16 @@
+const _ = require("lodash");
+const geo = require("./geo.json");
+const incomings = require("./incomings.json");
+
+// Из массива geo сделать объект "Федеральные округа" - ключ code (числовое значение), значение caption (название федерального округа)
+const createFederalDistricsObj = () => {
+  const obj = {};
+  _.forEach(geo,  (item) => {
+    return (obj[item.code] = item.caption);
+  });
+  return obj;
+};
+
+console.log(createFederalDistricsObj());
+
+
